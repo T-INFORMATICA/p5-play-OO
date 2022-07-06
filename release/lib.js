8d28d2a5d96107f60c4078172098b9a717acac9c
teacher-svb <teacher-svb@users.noreply.github.com>
2022-07-06 17:21:57 +0000

    concatenated lib files

--- release/lib.js
+++ release/lib.js
@@ -271,7 +271,13 @@ class Collider {
         return boundsPoint;
     }
 }
+/**
+ * Class that represents a Game. Extend this class to create a baseclass for your Game.
+ */
 class Game {
+    /**
+     * Creates a new Game instance.
+     */
     constructor() {
         GameManager.GetInstance().GameInstance = this;
     }
