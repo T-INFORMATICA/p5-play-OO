2d040f2e5de928052634df2dcce3db27c8164e8b
teacher-svb <39157063+teacher-svb@users.noreply.github.com>
2022-12-13 11:04:12 +0100

    Update CollisionGame.js

--- examples/collision/CollisionGame.js
+++ examples/collision/CollisionGame.js
@@ -6,6 +6,7 @@ class CollisionGame extends Game {
         super();
 
         this.#ball = new Ball(200, 200, 40, 40);
+        
         this.#wall = new Wall(200, 400, 200, 40);
         this.#wall = new Wall(200, 100, 200, 40);
     }
